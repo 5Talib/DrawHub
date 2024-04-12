d2b6fdbb5c75a2da992cdb11e389bfffb27a1071
5Talib <talib.khan.cer20@iitbhu.ac.in>
2024-04-12 11:30:32 +0530

    Updated socket

--- frontend/src/components/socket.js
+++ frontend/src/components/socket.js
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-// const URL = process.env.NODE_ENV === 'production' ? 'https://draw-hub-eta.vercel.app/' : 'https://draw-hub-eta.vercel.app/';
-const URL = "https://draw-hub-eta.vercel.app/";
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://draw-hub-eta.vercel.app/"
+    : "https://localhost:3001";
 
 export const socket = io(URL);
